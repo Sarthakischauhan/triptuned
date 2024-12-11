d1369ef20da2263a44afcf46aec40c000c0577b7
Sarthakischauhan <sarthakchauhann@gmail.com>
2024-12-10 21:17:48 -0800

    updated the visuals of the app

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import Navbar from "./components/Navbar";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -14,8 +15,9 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body
-        className={`antialiased`}
+        className={`antialiased w-3/5 m-auto`}
       >
+        <Navbar />
         {children}
       </body>
     </html>
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,15 +1,12 @@
-import SpotifySignInButton from "./components/spotifyButton";
-import LogoutButton from "./components/logoutButton";
 import { auth } from "auth/auth";
 
 export default async function Home() {
   const session = await auth();
-  console.log(session)
   return (
-    <div className="w-3/5 flex flex-col m-auto space-y-8 items-center">
+    <div className="flex flex-col m-auto space-y-8 items-center">
       <div className="mt-20 space-y-4 font-mono">
         <h1 className="text-3xl font-bold">
-          Welcome to triptuned
+          Welcome
         </h1>
         <p className="mb-3 text-left text-white-500 dark:text-white-400 leading-wide">
           Ever wanted to create spotify playlist for a drive you are planning? It could be a drive to work, home or even a cross country trip with your friends. 
@@ -27,9 +24,6 @@ export default async function Home() {
           <></>    
         }
       </div>
-      { session === null ? <SpotifySignInButton /> : <LogoutButton></LogoutButton>}
-
-
     </div>
   );
 }
